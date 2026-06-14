d7a6a15a04da85675ee67d020391eeb48c7d8378
sri4352 <srikant4352@gmail.com>
2026-06-14 12:22:11 +0530

    Update b.java

--- Arrays/Day1/b.java
+++ Arrays/Day1/b.java
@@ -2,9 +2,9 @@ package Array.Day_1;
 // Count zero in an array
 // GFG problem
 public class b {
-    int countZeroes(int arr[], int n) {
+    int countZeroes(int arr[]) {
         int no_of_zero =0;
-        for (int i =0;i<n;i++)
+        for (int i =0;i<arr.length;i++)
         {
             if (arr[i]==0)no_of_zero++;
         }
