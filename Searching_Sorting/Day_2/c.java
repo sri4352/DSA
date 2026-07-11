8ee2d27c1a213c2e8ec2afddee44a43743e923db
sri4352 <srikant4352@gmail.com>
2026-07-11 07:38:41 +0530

    Add method to find minimum in rotated sorted array
    Implement binary search to find the minimum in a rotated sorted array.

--- Searching_Sorting/Day_2/c.java
+++ Searching_Sorting/Day_2/c.java
@@ -1,1 +1,22 @@
+package Searching_Sorting.Day_2;
 
+public class c {
+    public int findMin(int[] nums) {
+        int i =0;
+        int j =nums.length-1;
+        while (i<j)
+        {
+            int mid =i+(j-i)/2;
+            if (nums[mid]>nums[j])
+            {
+                i =mid+1;
+            }
+            else j =mid;
+        }
+        return nums[j];
+    }
+    public static void main(String[] args) {
+
+    }
+
+}
