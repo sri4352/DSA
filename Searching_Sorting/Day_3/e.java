00fe07b300fbcfd10c0519e7484d2726d2106802
sri4352 <srikant4352@gmail.com>
2026-07-13 00:13:06 +0530

    Add guessNumber method for number guessing
    Implement binary search for guessing a number.

--- Searching_Sorting/Day_3/e.java
+++ Searching_Sorting/Day_3/e.java
@@ -1,1 +1,27 @@
+package Searching_Sorting.Day_3;
+// guess no (leetcode 374)
+public class e {
+    public int guess(int n)
+    {
+        return -1;
+    } // just for no error no use of this code
+       public int guessNumber(int n) {
+        int i =1;
+        int j =n;
+        while (i<=j)
+        {
+            int guess_no =i+(j-i)/2;
+            int result =guess(guess_no);
+            if (result==0)return guess_no;
+            else if (result==-1) {
+                j =guess_no-1;
+            }
+            else i =guess_no+1;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
 
+    }
+
+}
