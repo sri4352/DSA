ef22e0fc2933124fe414615229127fe63ea2d5d7
sri4352 <srikant4352@gmail.com>
2026-07-13 00:11:43 +0530

    Update d.java

--- Searching_Sorting/Day_2/d.java
+++ Searching_Sorting/Day_2/d.java
@@ -1,1 +1,20 @@
+package Searching_Sorting.Day_2;
+//Find Kth Rotation(GFG practice)
+public class d {
+    public int findKRotation(int arr[]) {
+        int i =0;
+        int j =arr.length-1;
+        while (i<j)
+        {
+            int mid =i+(j-i)/2;
+            if (arr[mid]<arr[j])j =mid;
+            else i =mid+1;
+        }
+        return i;
+    }
+    public static void main(String[] args) {
+
+    }
+
+}
 
