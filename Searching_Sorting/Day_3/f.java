1c01284c0ce41389b3221b06d84f5b7b2883446d
sri4352 <srikant4352@gmail.com>
2026-07-13 00:13:40 +0530

    Update f.java

--- Searching_Sorting/Day_3/f.java
+++ Searching_Sorting/Day_3/f.java
@@ -1,1 +1,50 @@
+package Searching_Sorting.Day_3;
+
+public class f {
+    // first approach
+    public int findKthPositive(int[] arr, int k) {
+        int num =1;
+        int n =arr.length;
+        int i =0;
+        while (i<n && k>0)
+        {
+            if (arr[i]==num)
+            {
+                i++;
+            }
+            else {
+                k--;
+            }
+            num++;
+        }
+       while (k!=0)
+       {
+           num++;
+           k--;
+       }
+        return num-1;
+    }
+
+    // second approach
+    public int findKthPositivee(int[] arr, int k) {
+       int i =0;
+       int j =arr.length-1;
+       while (i<=j)
+       {
+           int mid =i+(j-i)/2;
+           int no_of_missing =arr[mid]-(mid+1);
+           if (no_of_missing<k)
+           {
+               i =mid+1;
+           }
+           else j =mid-1;
+       }
+       return i+k;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
 
