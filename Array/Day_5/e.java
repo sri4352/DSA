891d766f2fa5a32542969b7aa9d518d5aab64158
sri4352 <srikant4352@gmail.com>
2026-07-17 02:29:35 +0530

    Update e.java

--- Array/Day_5/e.java
+++ Array/Day_5/e.java
@@ -1,1 +1,34 @@
+package Array.Day_5;
+// rotate image (leetcode 48)(clockwise)
+public class e {
+    public void rotate(int[][] matrix) {
+        int n =matrix.length;
+        for (int i =0;i<n;i++)
+        {
+            for (int j =i;j<n;j++)
+            {
+                int temp =matrix[i][j];
+                matrix[i][j] =matrix[j][i];
+                matrix[j][i] =temp;
+            }
+        }
+        for (int i =0;i<n;i++)
+        {
+            int j =0;
+            int k =n-1;
+            while (j<k)
+            {
+                int temp =matrix[i][j];
+                matrix[i][j] =matrix[i][k];
+                matrix[i][k] =temp;
+                j++;
+                k--;
+            }
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+
+}
 
