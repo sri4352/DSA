940aab21963d027fc18778ffb7b8b332a5d63319
sri4352 <srikant4352@gmail.com>
2026-07-16 23:59:29 +0530

    Update c.java

--- Array/Day_5/c.java
+++ Array/Day_5/c.java
@@ -1,1 +1,41 @@
+package Array.Day_5;
+
+import java.util.Arrays;
+
+// 3 sum closest (leetcode 16 and GFG practice)
+    public class c {
+    public int threeSumClosest(int[] nums, int target) {
+        int n =nums.length;
+        Arrays.sort(nums);
+        int closestSum =0;
+        int diff =Integer.MAX_VALUE;
+        for(int i =0;i<n-2;i++)
+        {
+            int j =i+1;
+            int k =n-1;
+            while (j<k)
+            {
+                int sum =nums[i]+nums[j]+nums[k];
+                if (sum==target)return sum;
+                if (Math.abs(target-sum)<diff)
+                {
+                    diff =Math.abs(target-sum);
+                    closestSum =sum;
+                }
+                if (Math.abs(target-sum)==diff)
+                {
+                    diff =Math.abs(target-sum);
+                    closestSum =Math.max(closestSum,sum);
+                }
+                if (sum<target)j++;
+                else k--;
+            }
+        }
+        return closestSum;
+    }
+    public static void main(String[] args) {
+
+    }
+
+}
 
