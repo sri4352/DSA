1192b6fbaa5c9c586dea79b0409ff3b49ed40530
sri4352 <srikant4352@gmail.com>
2026-07-19 11:54:55 +0530

    Update k.java

--- Array/Day_5/k.java
+++ Array/Day_5/k.java
@@ -1,1 +1,51 @@
 
+package Array.Day_5;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+//sort matrix by diagonals(leetcode 3446)
+public class k {
+    public int[][] sortMatrix(int[][] grid) {
+        int n = grid.length;
+        int [][] ans =new int[n][n];
+        HashMap<Integer, ArrayList<Integer>>map =new HashMap<>();
+        for (int i =0;i<n;i++)
+        {
+            for (int j =0;j<n;j++)
+            {
+                map.putIfAbsent(i-j,new ArrayList<>());
+                map.get(i-j).add(grid[i][j]);
+            }
+        }
+        for (int key :map.keySet())
+        {
+            Collections.sort(map.get(key));
+        }
+        for (int i =0;i<n;i++)
+        {
+            for (int j =0;j<n;j++)
+            {
+                ArrayList<Integer> arrayList =map.get(i-j);
+               if (i>=j)
+               {
+                   int last_element =arrayList.get(arrayList.size()-1);
+                   ans[i][j] =last_element;
+                   arrayList.remove(arrayList.size()-1);
+               }
+               else
+               {
+                   int element =arrayList.get(i);
+                   ans[i][j] =element;
+               }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
