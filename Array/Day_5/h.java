31cbb7ad7ad638a3231f6e2b7f371ea45a59f7b4
sri4352 <srikant4352@gmail.com>
2026-07-19 11:56:09 +0530

    Update h.java

--- Array/Day_5/h.java
+++ Array/Day_5/h.java
@@ -1,1 +1,23 @@
+package Array.Day_5;
+// container with most water(leetcdoe 11) (GFG practice)
+public class h {
+    public int maxArea(int[] height) {
+        if (height.length<=1)return 0;
+        int maxArea =Integer.MIN_VALUE;
+        int i =0;
+        int j =height.length-1;
+        while (i<j)
+        {
+            int area =(j-i)*Math.min(height[i],height[j]);
+            if (height[i]<height[j])i++;
+            else j--;
+            maxArea =Math.max(area,maxArea);
+        }
+        return maxArea;
+    }
+    public static void main(String[] args) {
+
+    }
+
+}
 
