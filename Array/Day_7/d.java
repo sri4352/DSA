71de9b6310d83ebcbd1e25c9186c7b0655581100
sri4352 <srikant4352@gmail.com>
2026-07-23 09:45:02 +0530

    Update d.java

--- Array/Day_7/d.java
+++ Array/Day_7/d.java
@@ -1,1 +1,27 @@
 
+package Array.Day_7;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+// kids with max candies (leetcode 1431)
+public class d {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> ans =new ArrayList<>();
+        int max = Integer.MIN_VALUE;
+        for (int i =0;i<candies.length;i++)
+        {
+            if (candies[i]>max)max =candies[i];
+        }
+        for (int i =0;i<candies.length;i++)
+        {
+            if (candies[i]+extraCandies>=max)ans.add(true);
+            else ans.add(false);
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+
+    }
+
+}
