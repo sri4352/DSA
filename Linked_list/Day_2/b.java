e9d6034979293f0c38886453dec61fed88704b3d
sri4352 <srikant4352@gmail.com>
2026-08-08 00:59:12 +0530

    Update b.java

--- Linked_list/Day_2/b.java
+++ Linked_list/Day_2/b.java
@@ -1,1 +1,39 @@
+package Linked_list.Day_2;
+//Insert in a Singly Linked List(GFG practice)
+public class b {
+    class Node {
+        int data;
+        Node next;
+
+        Node(int val) {
+            data = val;
+            next = null;
+        }
+    }
+    public Node insertPos(Node head, int pos, int val) {
+        if (pos == 1) {
+            Node newNode = new Node(val);
+            newNode.next = head;
+            head = newNode;
+            return head;
+        }
+        Node current =head;
+        for (int i = 1; i < pos && current!=null; i++)
+        {
+            current =current.next;
+        }
+        if (current==null)return head;
+        Node newNode =new Node(val);
+        newNode.next =current.next;
+        current.next =newNode;
+        return head;
+    }
+    public static void main(String[] args) {
+
+    }
+
+}
+
+
+
 
