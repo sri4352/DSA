bfcb78e11566d8dc3b04343055167324d25b9469
sri4352 <srikant4352@gmail.com>
2026-08-07 22:29:39 +0530

    Update a.java

--- Linked_list/Day_2/a.java
+++ Linked_list/Day_2/a.java
@@ -1,1 +1,25 @@
+package Linked_list.Day_2;
+// LinkedList Traversal(GFG practice)
+public class a {
+    class Node {
+        int data;
+        Node next;
 
+        Node(int val) {
+            data = val;
+            next = null;
+        }
+    }
+    void printList(Node head) {
+        Node temp =head;
+        while (temp!=null)
+        {
+            System.out.println(temp.data);
+            temp =temp.next;
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+
+}
