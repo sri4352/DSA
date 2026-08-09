45421ccd3d0387886273f9a7435cda8048c16f22
sri4352 <srikant4352@gmail.com>
2026-08-10 00:46:14 +0530

    Update a.java

--- Map/Day_1/a.java
+++ Map/Day_1/a.java
@@ -1,1 +1,32 @@
+package Map.Day_1;
+
+import java.util.HashMap;
+
+// continuous subarray sum(leetcode 523)
+public class a {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        HashMap<Integer,Integer> map =new HashMap<>();
+        map.put(0,-1);
+        int sum =0;
+        int n = nums.length;
+        for (int i =0;i<n;i++)
+        {
+            sum +=nums[i];
+            int remainder =sum%k;
+            if (map.containsKey(remainder))
+            {
+                if (i-map.get(remainder)>=2)
+                {
+                    return true;
+                }
+            }
+            else map.put(remainder,i);
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+
+    }
+
+}
 
