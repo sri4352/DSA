3413d3ee1caa8832bef6c5d19bb3f842fe4de6c5
sri4352 <srikant4352@gmail.com>
2026-08-17 01:32:37 +0530

    Update b.java

--- Map/Day_1/b.java
+++ Map/Day_1/b.java
@@ -1,1 +1,41 @@
+package Map.Day_1;
+
+import java.util.*;
+
+// find player with zero looses(leetcode 2225)
+public class b {
+    public List<List<Integer>> findWinners(int[][] matches) {
+        int n =matches.length;
+        HashMap<Integer,Integer> map =new HashMap<>();
+        for (int i =0;i<n;i++)
+        {
+            map.put(matches[i][1], map.getOrDefault(matches[i][1],0)+1);
+        }
+        List<Integer> no_loose =new ArrayList<>();
+        List<Integer> one_loose =new ArrayList<>();
+        for (int i =0;i<n;i++)
+        {
+           if (map.get(matches[i][1])==1)
+           {
+               one_loose.add(matches[i][1]);
+           }
+           if (!map.containsKey(matches[i][0]))
+           {
+               no_loose.add(matches[i][0]);
+               map.put(matches[i][0],2);
+           }
+        }
+        Collections.sort(no_loose);
+        Collections.sort(one_loose);
+        List<List<Integer>> ans_list =new ArrayList<>();
+        ans_list.add(no_loose);
+        ans_list.add(one_loose);
+        return ans_list;
+    }
+    public static void main(String[] args) {
+
+    }
+
+}
+
 
