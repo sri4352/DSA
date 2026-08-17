6be52450aa9955a0c0b25b08460c026e7aaa5240
sri4352 <srikant4352@gmail.com>
2026-08-18 01:10:51 +0530

    Add numIdenticalPairs method in class e
    Implement method to count identical pairs in an array.

--- Map/Day_1/e.java
+++ Map/Day_1/e.java
@@ -1,1 +1,29 @@
+package Map.Day_1;
+
+import java.util.HashMap;
+
+// no of good pattern(leetcode 1512)
+public class e {
+    public int numIdenticalPairs(int[] nums) {
+        int result =0;
+        int n =nums.length;
+        HashMap<Integer,Integer> hashMap =new HashMap<>();
+        for (int i =0;i<n;i++)
+        {
+            if (!hashMap.containsKey(nums[i]))
+            {
+                hashMap.put(nums[i],1);
+            }
+            else {
+                result += hashMap.get(nums[i]);
+                hashMap.put(nums[i], hashMap.get(nums[i]) + 1);
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+
+    }
+
+}
 
