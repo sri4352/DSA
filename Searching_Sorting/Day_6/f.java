ae5d4a410e2d80816bc6d23948ac991f8ef59252
sri4352 <srikant4352@gmail.com>
2026-08-19 07:39:32 +0530

    Update f.java

--- Searching_Sorting/Day_6/f.java
+++ Searching_Sorting/Day_6/f.java
@@ -1,1 +1,16 @@
+package Searching_Sorting.Day_6;
+//Smallest Missing Number(GFG practice)
+public class f {
 
+    public int findFirstMissing(int[] arr) {
+        int i =0;
+        int j =arr.length-1;
+        while (i<=j)
+        {
+            int mid =i+(j-i)/2;
+            if (arr[mid]==mid)i =mid+1;
+            else j =mid-1;
+        }
+        return i;
+    }
+}
