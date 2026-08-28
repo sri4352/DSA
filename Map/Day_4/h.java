3e4a51fa9a5ad724273383aaf05f7ec7e2149b26
sri4352 <srikant4352@gmail.com>
2026-08-28 23:41:15 +0530

    Update h.java

--- Map/Day_4/h.java
+++ Map/Day_4/h.java
@@ -1,1 +1,25 @@
+package Map.Day_4;
+
+// leetcode 3120
+
+public class h {
+    public int numberOfSpecialChars(String word) {
+        int[] arr =new int[52];
+        for (int i =0;i<word.length();i++)
+        {
+            char ch =word.charAt(i);
+            if (Character.isLowerCase(ch))
+            {
+                arr[ch-'a']++;
+            }
+            else arr[ch-'A'+26]++;
+        }
+        int count =0;
+        for (int i =0;i<26;i++)
+        {
+            if (arr[i]>0 && arr[i+26]>0)count++;
+        }
+        return count;
+    }
+}
 
