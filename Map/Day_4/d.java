dfb4e66dcccf9796037c2ba7bdc5d01233559285
sri4352 <srikant4352@gmail.com>
2026-08-28 19:39:54 +0530

    Update d.java

--- Map/Day_4/d.java
+++ Map/Day_4/d.java
@@ -1,1 +1,27 @@
+package Map.Day_4;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// leetcode 898
+public class d {
+    public int subarrayBitwiseORs(int[] arr) {
+        Set<Integer> prev =new HashSet<>();
+        Set<Integer> curr =new HashSet<>();
+        Set<Integer> result =new HashSet<>();
+        for (int i :arr)
+        {
+            result.add(i);
+            curr.add(i);
+            for (int x :prev)
+            {
+                result.add(i|x);
+                curr.add(i | x);
+            }
+            prev =curr;
+            curr =new HashSet<>();
+        }
+        return result.size();
+    }
+}
 
