f11212c43cccec6db8f7513c8669463e3f276ac7
Sri Kant Kumar <srikant4352@gmail.com>
2026-08-31 09:19:10 +0530

    Update a.java

--- Linked_list/Day_4/a.java
+++ Linked_list/Day_4/a.java
@@ -1,1 +1,32 @@
+package Linked_list.Day_4;
+
+// leetcode 382
+
+import java.util.ArrayList;
+import java.util.Random;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int d) {
+        this.val = d;
+        this.next = null;
+    }
+    ArrayList<Integer>arrayList =new ArrayList<>();
+    public Solution(ListNode head) {
+        ListNode temp =head;
+        while (temp!=null)
+        {
+            arrayList.add(temp.val);
+            temp =temp.next;
+        }
+    }
+    public int getRandom() {
+        int n =arrayList.size();
+        Random random =new Random();
+        int index = random.nextInt(n);
+        return arrayList.get(index);
+    }
+}
+
 
